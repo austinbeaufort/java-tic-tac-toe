c218feeabf476e6182875a79b9af841739454107
austinbeaufort <austinbeaufort7@gmail.com>
2020-07-17 19:38:19 -0400

    adding ability to places X's onto the gameboard

--- src/com/tictactoe/TicTacToe.java
+++ src/com/tictactoe/TicTacToe.java
@@ -1,5 +1,7 @@
 package com.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
 
 	public static void main(String[] args) {
@@ -12,7 +14,19 @@ public class TicTacToe {
 		};
 		
 		printGameBoard(gameBoard);
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your placement (1-9):");
+		final int position = scan.nextInt();
+		
+		System.out.println(position);
+		scan.close();
+		
+		placePiece(gameBoard, position, "player");
+		
+		printGameBoard(gameBoard);
 	}
+	
 	
 	
 	public static void printGameBoard(char[][] gameBoard) {
@@ -21,6 +35,50 @@ public class TicTacToe {
 				System.out.print(c);
 			System.out.println();
 		}
+	}
+	
+	
+	
+	
+	public static void placePiece(char[][] gameBoard, int position, String user) {
+		
+		char symbol = ' ';
+		
+		if (user.equals("player"))
+			symbol = 'X';
+		else if (user.equals("cpu"))
+			symbol = '0';
+		
+		
+		switch(position) {
+			case 1:
+				gameBoard[0][0] = symbol;
+				break;
+			case 2:
+				gameBoard[0][2] = symbol;
+				break;
+			case 3:
+				gameBoard[0][4] = symbol;
+				break;
+			case 4:
+				gameBoard[2][0] = symbol;
+				break;
+			case 5:
+				gameBoard[2][2] = symbol;
+				break;
+			case 6:
+				gameBoard[2][4] = symbol;
+				break;
+			case 7:
+				gameBoard[4][0] = symbol;
+				break;
+			case 8:
+				gameBoard[4][2] = symbol;
+				break;
+			case 9:
+				gameBoard[4][4] = symbol;
+				break;
+			}
 	}
 
 }
